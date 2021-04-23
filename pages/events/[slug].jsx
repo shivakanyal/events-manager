13f0d645b3a9ec5b91b94c4d21c4b87bc5913c82
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-04-23 17:46:10 +0530

    Details page is completed.

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,14 +1,49 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
 
-import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 
 export default function EventPage({ evt }) {
-  const router = useRouter();
+  const deleteEvent = (e) => {
+    console.log("deleted.");
+  };
+
   return (
     <Layout>
-      <h1>{evt.name}</h1>
-      <h3>{router.query.slug}</h3>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+        <h3>Performers:</h3>
+        <p>{evt.performance}</p>
+        <h3>DesCription :</h3>
+        <p>{evt.description}</p>
+        <h3>Venue:{evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
@@ -17,7 +52,7 @@ export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/api/events/${slug}`);
 
   const events = await res.json();
-
+  console.log("events", events[0]);
   return {
     props: {
       evt: events[0],
