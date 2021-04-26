53b15652047fce8c301ca3493fc39a346422b4ad
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-04-26 10:41:03 +0530

    User Persistance is done.

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -9,6 +9,9 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+  useEffect(() => checkUserLoggedIn(), []);
+
   // Register User
   const register = async (user) => {
     console.log(user);
@@ -30,6 +33,7 @@ export const AuthProvider = ({ children }) => {
     console.log(data);
     if (res.ok) {
       setUser(data.user);
+      router.push("/account/dashboard");
     } else {
       setError(data.message);
       setError(null);
@@ -42,7 +46,13 @@ export const AuthProvider = ({ children }) => {
   };
   // Check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log("Check..");
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return (
--- pages/api/login.jsx
+++ pages/api/login.jsx
@@ -1,5 +1,5 @@
 import { API_URL } from "@/config/index";
-
+import cookie from "cookie";
 export default async (req, res) => {
   if (req.method === "POST") {
     const { identifier, password } = req.body;
@@ -17,7 +17,17 @@ export default async (req, res) => {
     const data = await strapiRes.json();
     console.log("strapi res", data);
     if (strapiRes.ok) {
-      // Set Cookie
+      // Setting Cookie
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 24 * 7, // 1 Week
+          sameSite: "strict",
+          path: "/",
+        })
+      );
       res.status(200).json({ user: data.user });
     } else {
       res
