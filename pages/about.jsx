9041f8aa87537592aec2771297b2d8a82e191626
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-04-27 14:31:22 +0530

    little Refactoring is done.

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,8 +4,9 @@ export default function AboutPage() {
   return (
     <Layout title="About">
       <h1>About</h1>
-      <p>This is an app to find the latest events and other musical events</p>
+      <p>This is an app to find the latest events happening in our college</p>
       <p>Version 1.0.0</p>
+      <p>Created by Shiva Kanyal</p>
     </Layout>
   );
 }
